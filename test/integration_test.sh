628eec1b72c7c1911eeabf1e5dbc42c7173c6a15
Vedprakash Tanwar <vedtanwar01@gmail.com>
2026-06-09 19:08:29 +0530

    fix integration test

--- test/integration_test.sh
+++ test/integration_test.sh
@@ -1,52 +1,27 @@
-# #!/bin/bash
-
-# set -e
-
-# echo "Starting TCP Server..."
-
-# ./build/bin/server &
-# SERVER_PID=$!
-
-# # Give server time to start
-# sleep 2
-
-# echo "Running TCP Client (sending message to server)..."
-
-# # Send message to client via stdin
-# OUTPUT=$(echo "Hello Server" | ./build/bin/client)
-
-# echo "Client Output:"
-# echo "$OUTPUT"
-
-# echo "Stopping server..."
-# kill $SERVER_PID
-
-# echo "Integration test PASSED"
-
-
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
 echo "Starting TCP Server..."
-
 ./build/bin/server &
 SERVER_PID=$!
+
+trap 'kill $SERVER_PID' EXIT
 
 sleep 2
 
 echo "Running TCP Client with Expect (TTY simulation)..."
 
-expect << EOF
+expect <<'EOF'
 spawn ./build/bin/client
 
 expect "Enter the Message for the Server"
 send "Hello Server\r"
 
+expect "Enter the Message for the Server"
+send "quit\r"
+
 expect eof
 EOF
-
-echo "Stopping server..."
-kill $SERVER_PID
 
 echo "Integration test PASSED"
